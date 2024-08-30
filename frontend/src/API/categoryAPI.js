2b2657bf03c71f5b5ad7913a4c8ba871f489fc11
ZiadHegazy <ziadayman9901@gmail.com>
2024-08-30 14:13:58 +0300

    last

--- frontend/src/API/categoryAPI.js
+++ frontend/src/API/categoryAPI.js
@@ -1,6 +1,6 @@
 const api=process.env.API?process.env.API:'http://localhost:8000';
-
 export const createCategory=async (parentId)=>{
+    console.log(api);
     const response=await fetch(`${api}/createCategory`,{
         method:'POST',
         headers:{
@@ -12,6 +12,7 @@ export const createCategory=async (parentId)=>{
         
 }
 export const getCategories=async ()=>{
+    console.log(api);
     const response=await fetch(`${api}/getCategories`);
     return await response.json();
 }
